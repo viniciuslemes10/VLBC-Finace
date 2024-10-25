ad4ae47148e658aa2fce70cc8ab27b4c22239f4d
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-10-25 15:02:28 -0300

    Initial project with settings

---
+++ src/main/java/com/br/vlbc/VlbcApplication.java
@@ -0,0 +1,13 @@
+package com.br.vlbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/vlbc/VlbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.vlbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
