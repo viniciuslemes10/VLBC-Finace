c2385352b93c02d90d45f5bd20ff8ca08be32f33
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-05 14:25:26 -0300

    feat: created dto for details

---
+++ src/main/java/com/br/vlbc/records/UserDetailsDTO.java
@@ -0,0 +1,32 @@
+package com.br.vlbc.records;
+
+import com.br.vlbc.model.User;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record UserDetailsDTO(
+        Long id,
+        String userName,
+        String fullName,
+        String email,
+        String password,
+        BigDecimal balance,
+        boolean accountNonExpired,
+        boolean accountNonLocked,
+        boolean credentialsNonExpired,
+        boolean enable
+) {
+    public UserDetailsDTO(User user) {
+        this(user.getId(), user.getUserName(), user.getFullName(),
+                user.getEmail(), user.getPassword(), user.getBalance(),
+                user.isAccountNonExpired(), user.isAccountNonLocked(),
+                user.isCredentialsNonExpired(), user.isEnable());
+    }
+
+    public static List<UserDetailsDTO> fromUsers(List<User> users) {
+        return users.stream()
+                .map(UserDetailsDTO::new)
+                .toList();
+    }
+}
