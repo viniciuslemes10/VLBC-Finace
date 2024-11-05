4ff70c3400d0feb24e694ec02615af28b1443699
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-05 15:17:09 -0300

    feat: created permission repository

---
+++ src/main/java/com/br/vlbc/repositories/PermissionsRepository.java
@@ -0,0 +1,12 @@
+package com.br.vlbc.repositories;
+
+import com.br.vlbc.model.Permissions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PermissionsRepository extends JpaRepository<Permissions, Long> {
+    Optional<Permissions> findByDescription(String description);
+}
