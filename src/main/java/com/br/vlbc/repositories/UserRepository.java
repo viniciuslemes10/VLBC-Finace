24b34c0669c921200bb43ee81402f5dbddd00549
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-05 13:37:36 -0300

    feat: criando repository de users

---
+++ src/main/java/com/br/vlbc/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.br.vlbc.repositories;
+
+import com.br.vlbc.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+}
