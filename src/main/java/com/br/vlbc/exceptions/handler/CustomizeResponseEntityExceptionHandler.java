0322117881dc259bec5633d1c3d71c6876ee75a4
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-05 14:24:49 -0300

    feat: add exceptions customized

--- src/main/java/com/br/vlbc/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
+++ src/main/java/com/br/vlbc/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.br.vlbc.exceptions.handler;
 
-import com.br.vlbc.exceptions.CategoryExistException;
-import com.br.vlbc.exceptions.CategoryNotFoundException;
-import com.br.vlbc.exceptions.ExceptionResponse;
-import com.br.vlbc.exceptions.InvalidTypeException;
+import com.br.vlbc.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -49,6 +46,26 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
 
     @ExceptionHandler(InvalidTypeException.class)
     public final ResponseEntity<ExceptionResponse> handlerInvalidTypeArgumentException(Exception ex, WebRequest req) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                req.getDescription(false)
+        );
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handlerUserNotFoundException(Exception ex, WebRequest req) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                req.getDescription(false)
+        );
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BalanceInvalidException.class)
+    public final ResponseEntity<ExceptionResponse> handlerBalanceInvalidException(Exception ex, WebRequest req) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
