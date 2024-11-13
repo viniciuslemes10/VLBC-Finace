d2d2dc8b97e8db16aa5cbfe326b3f7777922b67e
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-13 11:11:50 -0300

    feat: creating transaction repository interface

---
+++ src/main/java/com/br/vlbc/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.br.vlbc.repositories;
+
+import com.br.vlbc.model.Transactions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transactions, Long> {
+}
