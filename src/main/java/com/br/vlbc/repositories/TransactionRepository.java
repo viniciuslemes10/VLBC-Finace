0ca693b47e5e08433a4be223a91194db7f804aaf
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-13 12:24:38 -0300

    feat: creating método para mostar todas as transções do usuário

--- src/main/java/com/br/vlbc/repositories/TransactionRepository.java
+++ src/main/java/com/br/vlbc/repositories/TransactionRepository.java
@@ -2,8 +2,14 @@ package com.br.vlbc.repositories;
 
 import com.br.vlbc.model.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Long> {
+    @Query("SELECT t FROM Transactions t WHERE t.user = :idUser")
+    List<Transactions> findAllOfIdUser(@Param("idUser") Long idUser);
 }
